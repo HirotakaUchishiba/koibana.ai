b00eb30e414007cdea619b49c54bd9ceebd1a7e7
HirotakaUchishiba <hirotaka1999821@gmail.com>
2023-04-25 13:46:22 +0900

    Firebaseと連携

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -3,8 +3,14 @@
 import 'package:client/screens/home_screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -22,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.pink,
       ),
-      home: const HomeScreen(),
+      home:  HomeScreen(),
     );
   }
 }
--- client/lib/screens/home_screen/home_screen.dart
+++ client/lib/screens/home_screen/home_screen.dart
@@ -1,10 +1,10 @@
 //Package import
 //Project import
 import 'package:client/screens/signup_screen/signup_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
